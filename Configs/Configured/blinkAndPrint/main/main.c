e45eab88ad9c3df29c359dae8bbdb82c23de86b0
coder137 <coder137@gmail.com>
2018-03-18 22:30:45 +0530

    Minor Changes
    Minor Changes

--- Configs/Configured/blinkAndPrint/main/main.c
+++ Configs/Configured/blinkAndPrint/main/main.c
@@ -8,6 +8,8 @@
 const char *pcTextForTask1 = "Task 1\n";
 const char *pcTextForTask2 = "Task 2\n";
 
+const TickType_t xDelay250ms = pdMS_TO_TICKS(250);
+
 //TODO, Change the Project Name in Makefile
 //TODO, Test this with ESP32
 void blink_task(void *pvParameters)
@@ -26,10 +28,10 @@ void blink_task(void *pvParameters)
     {
         /* Blink off (output low) */
         gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(xDelay250ms);
         /* Blink on (output high) */
         gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(xDelay250ms);
     }
 
     vTaskDelete(NULL);
@@ -43,7 +45,7 @@ void print_task(void *pvParameters)
 	while(1)
 	{
 		printf("Parameter: %s", pcTaskName);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		vTaskDelay(xDelay250ms);
 	}
 
 	vTaskDelete(NULL);
